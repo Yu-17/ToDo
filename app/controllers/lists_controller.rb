2e57e1d44a2bf6abd4253637601246bdfd150012
Yu-17 <api.0204@gmail.com>
2022-07-17 00:05:26 +0900

    crud

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,19 +6,37 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to '/'
+    redirect_to list_path(list.id)
   end
 
   def index
     @lists = List.all
-
-
   end
 
   def show
     @list = List.find(params[:id])
   end
 
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to lists_path
+  end
+
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    respond_to do |format|
+      format.html { redirect_to lists_path, notice: "Task was successfully destroyed.", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
 
   private
   def list_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # get 'lists/new'
   # root 'top#index'
   # post 'lists' => 'lists#create'
